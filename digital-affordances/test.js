99331b7c0d2226c8d83048e721a67a2a7c0deaa3
Sarah <sarahcharlottekohl@gmail.com>
2021-03-07 01:32:46 -0600

    formatting and stuff

--- digital-affordances/test.js
+++ digital-affordances/test.js
@@ -1,15 +1,12 @@
-import testEmojis from '../digital-affordances/src/lib/affordances/emoji.js';
+import testEmojis from "../digital-affordances/src/lib/affordances/emoji.js";
 
 console.log(
-testEmojis('flowers').length, // false :)
+  testEmojis("flowers").length // false :)
 );
 console.log(
-
-    testEmojis('🌼🌺🌸flo 🌺 er 🌸 wers 🌼🌺🌸').length, // true :)
-
-    );
-    console.log(
-
-        testEmojis('flowers 123').length, // false :)
-        );
-console.log('end of line');
+  testEmojis("🌼🌺🌸flo 🌺 er 🌸 wers 🌼🌺🌸").length // true :)
+);
+console.log(
+  testEmojis("flowers 123").length // false :)
+);
+console.log("end of line");
