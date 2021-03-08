0b6282a314be671d62062e433e640403a53b4281
Sarah <sarahcharlottekohl@gmail.com>
2021-03-07 21:28:18 -0600

    accept thresholds

--- digital-affordances/src/lib/affordances/emoji.js
+++ digital-affordances/src/lib/affordances/emoji.js
@@ -1,4 +1,10 @@
 // /\p{Extended_Pictographic}/u.test('flowers'), // false :)
 // /\p{Extended_Pictographic}/u.test('flowers 🌼🌺🌸'), // true :)
 // /\p{Extended_Pictographic}/u.test('flowers 123'), // false :)
-export default (string) => string.match(/\p{Extended_Pictographic}/gu) || [];
+export default (string, { threshold, density }) => {
+  const hits = string.match(/\p{Extended_Pictographic}/gu) || [];
+  if (threshold) {
+    return hits.length >= threshold;
+  }
+  return;
+};
--- digital-affordances/test.js
+++ digital-affordances/test.js
@@ -1,12 +1,8 @@
 import testEmojis from "../digital-affordances/src/lib/affordances/emoji.js";
 
+console.log(testEmojis("flowers", { threshold: 1 })); //false
+console.log(testEmojis("🌼🌺🌸flo 🌺 er 🌸 wers 🌼🌺🌸", { threshold: 8 })); //true
+console.log(testEmojis("🌼🌺🌸flo 🌺 er 🌸 wers 🌼🌺🌸", { threshold: 9 })); //false
 console.log(
-  testEmojis("flowers").length // false :)
+  testEmojis("flowers 123", { threshold: 1 }) //false
 );
-console.log(
-  testEmojis("🌼🌺🌸flo 🌺 er 🌸 wers 🌼🌺🌸").length // true :)
-);
-console.log(
-  testEmojis("flowers 123").length // false :)
-);
-console.log("end of line");
